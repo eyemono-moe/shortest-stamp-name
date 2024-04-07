c2e424e7710f37a4f4d57e1e417efefa22dbc9d0
eyemono-moe <ysdr83@gmail.com>
2024-04-07 09:58:25 +0900

    fetchを並列に

--- src/libs/calcKimariji.ts
+++ src/libs/calcKimariji.ts
@@ -4,12 +4,14 @@
 const getAltnameTable = () =>
   fetch(
     "https://raw.githubusercontent.com/traPtitech/traQ_S-UI/master/src/assets/emoji_altname_table.json",
-  ).then(
-    (response) =>
-      response.json() as Promise<{
-        altNameTable: Record<string, string>;
-      }>,
-  );
+  )
+    .then(
+      (response) =>
+        response.json() as Promise<{
+          altNameTable: Record<string, string>;
+        }>,
+    )
+    .then((data) => data.altNameTable);
 
 /**
  * スタンプ一覧を取得(unicode, traPオリジナルすべて)
@@ -64,8 +66,10 @@ export const calcKimariji = async () => {
   if (TOKEN === undefined) {
     throw new Error("TOKEN is not defined");
   }
-  const stamps = await getStamps(TOKEN);
-  const altNames = (await getAltnameTable()).altNameTable;
+  const [stamps, altNames] = await Promise.all([
+    getStamps(TOKEN),
+    getAltnameTable(),
+  ]);
 
   // 辞書順にソート
   const sortedStamps = stamps.sort((a, b) => a.localeCompare(b));
